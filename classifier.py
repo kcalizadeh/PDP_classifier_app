41990abef40c6574c6da704eebbb535689969c08
Kourosh Alizadeh <kcalizadeh@gmail.com>
2021-01-22 14:42:15 -0700

    textarea

--- classifier.py
+++ classifier.py
@@ -108,6 +108,7 @@ twitter_bar = html.Div(id="twitter-bar-container", children=
                     placeholder="enter a twitter username to search",
                     type='text',
                     n_submit=0,
+                    autoFocus=True
                     style={'width': '100%'},), width=8),
         dbc.Col(dbc.Button("SUBMIT", id="twitter-bar-submit-button", color="primary", className="mr-1", n_clicks=0), width=2)
     ])
@@ -117,10 +118,10 @@ twitter_bar = html.Div(id="twitter-bar-container", children=
 # search bar object
 search_bar = html.Div(id="classification-bar-container", children=
     [
-        dbc.Input(id="classification-bar", 
+        dcc.Textarea(id="classification-bar", 
                     placeholder="Enter text to classify - the more you can provide, the more accurate the classification will be. \n\nNote that some very common words will be removed from your submission to improve classification accuracy.",
-                    type='text',
-                    n_submit=0,
+                    # type='text',
+                    # n_submit=0,
                     style={'width': '100%', 'height': 100, 'vertical-align': 'top', 'text-align': 'left'},),
         dbc.Button("SUBMIT", id="classification-bar-submit-button", color="primary", className="mr-1", n_clicks=0)
     ])
@@ -206,15 +207,15 @@ def generate_explainer_html(n_clicks, n_submit, username,
 
 # callback for search bar
 @app.callback(Output(component_id="classification-bar-output", component_property="children"),
-              [Input(component_id="classification-bar-submit-button", component_property="n_clicks"),
-              Input(component_id="classification-bar", component_property="n_submit")],
+              [Input(component_id="classification-bar-submit-button", component_property="n_clicks")],
+            #   Input(component_id="classification-bar", component_property="n_submit")],
               [State(component_id="classification-bar", component_property="value")])
 def generate_explainer_html(n_clicks, n_submit, text, 
                             # model=nb_model, vectorizer=vectorizer,
                             class_names = [name.replace('_', ' ').title() for name in list(school_label_dict.keys())]):
-    if n_clicks < 1 and n_submit < 1:
+    if n_clicks < 1:# and n_submit < 1:
         return [html.Br(), html.P('The classification can take some time. Please be patient, and your text classification will appear here when it is ready.')]
-    if n_clicks > 0 or n_submit > 0:
+    if n_clicks > 0:# or n_submit > 0:
         with open('NB_model.pkl', 'rb') as f:
             nb_model = pickle.load(f)
         with open('vectorizer.pkl', 'rb') as f:
