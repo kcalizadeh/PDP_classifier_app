151bde6a442f0f663c7462f01b85d46bc451ec3b
Kourosh Alizadeh <kcalizadeh@gmail.com>
2021-01-22 13:16:55 -0700

    rename

--- classifier.py
+++ classifier.py
@@ -147,7 +147,7 @@ def render_content(tab):
               Input(component_id="twitter-bar", component_property="n_submit")],
               [State(component_id="twitter-bar", component_property="value")])
 def generate_explainer_html(n_clicks, n_submit, username, 
-                            api=api, model=nb_model, vectorizer=vectorizer,
+                            # api=api, model=nb_model, vectorizer=vectorizer,
                             class_names = [name.replace('_', ' ').title() for name in list(school_label_dict.keys())]):
     if n_clicks < 1 and n_submit < 1:
         return [html.Br(), html.P('The classification can take some time. Please be patient, and your text classification will appear here when it is ready.')]
@@ -168,7 +168,7 @@ def generate_explainer_html(n_clicks, n_submit, username,
             #                                 num_features=10, 
             #                                 labels=[0,1,2,3,4,5,6,7,8,9],
             #                                 top_labels=3)
-            exp = classify_text(text, model, vectorizer, class_names=class_names)
+            exp = classify_text(text, nb_model, vectorizer, class_names=class_names)
             obj = html.Iframe(
                 srcDoc=exp.as_html(),
                 width='100%',
@@ -187,7 +187,7 @@ def generate_explainer_html(n_clicks, n_submit, username,
               Input(component_id="classification-bar", component_property="n_submit")],
               [State(component_id="classification-bar", component_property="value")])
 def generate_explainer_html(n_clicks, n_submit, text, 
-                            model=nb_model, vectorizer=vectorizer,
+                            # model=nb_model, vectorizer=vectorizer,
                             class_names = [name.replace('_', ' ').title() for name in list(school_label_dict.keys())]):
     if n_clicks < 1 and n_submit < 1:
         return [html.Br(), html.P('The classification can take some time. Please be patient, and your text classification will appear here when it is ready.')]
@@ -207,7 +207,7 @@ def generate_explainer_html(n_clicks, n_submit, text,
             #                                 num_features=10, 
             #                                 labels=[0,1,2,3,4,5,6,7,8,9],
             #                                 top_labels=3)
-            exp = classify_text(text, model, vectorizer, class_names=class_names)
+            exp = classify_text(text, nb_model, vectorizer, class_names=class_names)
             obj = html.Iframe(
                 srcDoc=exp.as_html(),
                 width='100%',
